31261ec35b22882899cd5328f042ce97a95a95f6
Rahul Raj Mogili <mogilirahulraj@gmail.com>
2021-06-29 11:44:30 -0400

    modified the code and added better styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import "./App.css";
+import NetflixLogo from "./images/netflix_logo.png";
 import Row from "./Row";
 import requests from "./requests";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hey! Let's build netflix clone.</h1>
-
-      <Row title="Popular on Netflix" fetchURL = {requests.fetchPopular}></Row>
-      <Row title="Top Rated Movies" fetchURL = {requests.fetchTopRated}></Row>
-      <Row title="Popular Movies" fetchURL = {requests.fetchPopular}></Row>
-      <Row title="Trending Movies" fetchURL = {requests.fetchTrending}></Row>
-      <Row title="Top Rated TV" fetchURL = {requests.fetchTopRatedTV}></Row>
+      <img src={NetflixLogo} alt="Neflix clone" class="logo__netflix" />
+      <Row
+        title="Popular on Netflix"
+        fetchURL={requests.fetchPopular}
+        isLargeRow
+      ></Row>
+      {/* here isLargeRow is true*/}
+      <Row title="Top Rated Movies" fetchURL={requests.fetchTopRated}></Row>
+      <Row title="Popular Movies" fetchURL={requests.fetchPopular}></Row>
+      <Row title="Trending Movies" fetchURL={requests.fetchTrending}></Row>
+      <Row title="Top Rated TV" fetchURL={requests.fetchTopRatedTV}></Row>
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,33 +1,40 @@
-import { React, useState, useEffect } from 'react'
-import axios from './axios';
-import './Row.css'
+import { React, useState, useEffect } from "react";
+import axios from "./axios";
+import "./Row.css";
 
 const base_url = "http://image.tmdb.org/t/p/original/";
 
-function Row({ title, fetchURL }) {
-	const [movies, setMovies] = useState([]);
+function Row({ title, fetchURL, isLargeRow }) {
+  const [movies, setMovies] = useState([]);
 
-	useEffect(() => {
-		async function fetchData() {
-			const request = await axios.get(fetchURL);	
-            setMovies(request.data.results)
-			return request
-        }
-		fetchData();
-	}, [fetchURL])		// fetchURL is used as a dependency to tell the useEffect that it is a variable outside the block
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchURL);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchURL]); // fetchURL is used as a dependency to tell the useEffect that it is a variable outside the block
 
-	console.log(movies)
-    return (
-			<div className="row">
-            <h2>{title}</h2>
-            
-            <div className="row__posters">
-                {movies.map(movie => (
-                    <img key={movie.id} src={`${base_url}${movie.poster_path}`} alt={movie.name} className="row__poster"/>
-                ))}
-            </div>
-			</div>
-    )
+  console.log(movies);
+  return (
+    <div className="row">
+      <h2>{title}</h2>
+
+      <div className="row__posters">
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
+            alt={movie.name}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
-export default Row
+export default Row;
